1414c2564ec585100dd690ce97c18426e1af01be
Sajitha-P <sajitharajendran@gmail.com>
2024-10-15 20:11:41 +0530

    Add files via upload

---
+++ degre.py
@@ -0,0 +1,162 @@
+import numpy as np
+class DegreeGLRLM:
+    def __init__(self, mat_0, mat_45, mat_90, mat_135):
+        self.Mat_0 = mat_0 
+        self.Mat_45 = mat_45
+        self.Mat_90 = mat_90
+        self.Mat_135 = mat_135
+        self.Degrees = [mat_0, mat_45, mat_90, mat_135]
+
+class Degree:
+    def __init__(self):
+        self.__image_src = None
+        self.__gray_level = None
+        self.__run_length = None
+        
+    def __degree0GLRLM(self):
+        degree0Matrix = np.zeros([self.__gray_level, self.__run_length])
+        counter = 0
+        for y in range(self.__image_src.shape[0]):
+            for x in range(self.__image_src.shape[1]):
+                nowVal = self.__image_src[y][x]
+                if x + 1 >= self.__image_src.shape[1]:
+                    nextVal = None
+                else:
+                    nextVal = self.__image_src[y][x + 1]
+                if nextVal != nowVal and counter == 0:
+                    degree0Matrix[int(nowVal)][counter] += 1
+                elif nextVal == nowVal:
+                    counter += 1
+                elif nextVal != nowVal and counter != 0:
+                    degree0Matrix[int(nowVal)][counter] += 1
+                    counter = 0
+        return degree0Matrix
+
+    def __degree90GLRLM(self):
+        degree90Matrix = np.zeros([self.__gray_level, self.__run_length])
+        counter = 0
+        for x in range(self.__image_src.shape[1]):
+            for y in range(self.__image_src.shape[0]):
+                nowVal = self.__image_src[y][x]
+                if y + 1 >= self.__image_src.shape[0]:
+                    nextVal = None
+                else:
+                    nextVal = self.__image_src[y + 1][x]
+                if nextVal != nowVal and counter == 0:
+                    degree90Matrix[int(nowVal)][counter] += 1
+                elif nextVal == nowVal:
+                    counter += 1
+                elif nextVal != nowVal and counter != 0:
+                    degree90Matrix[int(nowVal)][counter] += 1
+                    counter = 0
+        return degree90Matrix
+
+    def __degree45GLRLM(self):
+        degree45Matrix = np.zeros([self.__gray_level, self.__run_length])
+        for y in range(self.__image_src.shape[0]):
+            counter = 0
+            i_range = max(self.__image_src.shape)
+            for i in range(i_range):
+                y1 = y - i
+                if i >= self.__image_src.shape[1] or y1 < 0:
+                    break
+                else:
+                    nowVal = self.__image_src[y1][i]
+                if y1 - 1 < 0 or i + 1 >= self.__image_src.shape[1]:
+                    nextVal = None
+                else:
+                    nextVal = self.__image_src[y1 - 1][i + 1]
+                if nextVal != nowVal and counter == 0:
+                    degree45Matrix[int(nowVal)][counter] += 1
+                elif nextVal == nowVal:
+                    counter += 1
+                elif nextVal != nowVal and counter != 0:
+                    degree45Matrix[int(nowVal)][counter] += 1
+                    counter = 0
+        for x in range(self.__image_src.shape[1]):
+            if x == self.__image_src.shape[1] - 1:
+                break
+            counter = 0
+            i_range = max(self.__image_src.shape)
+            for i in range(i_range):
+                y_i = -1 - i
+                x_i = -1 + i - x
+                # print(y_i, x_i)
+                if x_i >= 0 or y_i <= -1 - self.__image_src.shape[0]:
+                    break
+                else:
+                    nowVal = self.__image_src[y_i][x_i]
+
+                if y_i - 1 <= -(self.__image_src.shape[0] + 1) or x_i + 1 >= 0:
+                    nextVal = None
+                else:
+                    nextVal = self.__image_src[y_i - 1][x_i + 1]
+                if nextVal != nowVal and counter == 0:
+                    degree45Matrix[int(nowVal)][counter] += 1
+                elif nextVal == nowVal:
+                    counter += 1
+                elif nextVal != nowVal and counter != 0:
+                    degree45Matrix[int(nowVal)][counter] += 1
+                    counter = 0
+        return degree45Matrix
+
+    def __degree135GLRLM(self):
+        degree135Matrix = np.zeros([self.__gray_level, self.__run_length])
+
+        for y in range(self.__image_src.shape[0]):
+            counter = 0
+            i_range = max(self.__image_src.shape)
+            for i in range(i_range):
+                y1 = y + i
+                if y1 >= self.__image_src.shape[0] or i >= self.__image_src.shape[1]:
+                    break
+                else:
+                    nowVal = self.__image_src[y1][i]
+                    if y1 >= self.__image_src.shape[0] - 1 or i >= self.__image_src.shape[1] - 1:
+                        nextVal = None
+                    else:
+                        nextVal = self.__image_src[y1 + 1][i + 1]
+                    if nextVal != nowVal and counter == 0:
+                        degree135Matrix[int(nowVal)][counter] += 1
+                    elif nextVal == nowVal:
+                        counter += 1
+                    elif nextVal != nowVal and counter != 0:
+                        degree135Matrix[int(nowVal)][counter] += 1
+                        counter = 0
+        for x in range(self.__image_src.shape[1]):
+            if x == 0:
+                continue
+            i_range = max(self.__image_src.shape)
+            counter = 0
+            for i in range(i_range):
+                x1 = x + i
+                if i >= self.__image_src.shape[0] or x1 >= self.__image_src.shape[1]:
+                    break
+                else:
+                    nowVal = self.__image_src[i][x1]
+                if i >= self.__image_src.shape[0] - 1 or x1 >= self.__image_src.shape[1] - 1:
+                    nextVal = None
+                else:
+                    nextVal = self.__image_src[i + 1][x1 + 1]
+                if nextVal != nowVal and counter == 0:
+                    degree135Matrix[int(nowVal)][counter] += 1
+                elif nextVal == nowVal:
+                    counter += 1
+                elif nextVal != nowVal and counter != 0:
+                    degree135Matrix[int(nowVal)][counter] += 1
+                    counter = 0
+        return degree135Matrix
+
+    
+    def create_matrix(self, normalized_img, level):
+        self.__image_src = normalized_img
+        self.__gray_level = level
+        self.__run_length = max(normalized_img.shape)
+        
+        mat0 = self.__degree0GLRLM()
+        mat45 = self.__degree45GLRLM()
+        mat90 = self.__degree90GLRLM()
+        mat135 = self.__degree135GLRLM()
+        
+        return DegreeGLRLM(mat0, mat45, mat90, mat135)
+
